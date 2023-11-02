2fa48d5253cc739c12065bb0ed5900d58fbb4a1f
Christian <christian.steckler@gmail.com>
2023-11-02 01:01:00 +0000

    updated invokeFunction and fixed openai integration

--- commands/fetchLogs.js
+++ commands/fetchLogs.js
@@ -3,11 +3,12 @@ import { hideBin } from 'yargs/helpers';
 const argv = yargs(hideBin(process.argv)).argv;
 import createBearerToken from '../utils/promises/createBearerToken.js';
 import fetchGfLogs from '../utils/fetch-logs.js';
+import fs from 'fs/promises'
 
 const fetchLogsCommand = async (functionId) => {
     const gliaBearerToken = await createBearerToken(process.env.GLIA_KEY_ID, process.env.GLIA_KEY_SECRET);
     const logs = await fetchGfLogs(gliaBearerToken, functionId);
-    console.log(logs)
+    await fs.writeFile('./logs.json', JSON.stringify(logs))
     return logs
 }
 
--- commands/invokeFunction.js
+++ commands/invokeFunction.js
@@ -4,11 +4,11 @@ const argv = yargs(hideBin(process.argv)).argv;
 import createBearerToken from '../utils/promises/createBearerToken.js';
 import invokeGliaFunction from '../utils/invoke-gf.js';
 
-const invokeFunctionCommand = async (invocationUri) => {
+const invokeFunctionCommand = async (invocationUri, data) => {
     const gliaBearerToken = await createBearerToken(process.env.GLIA_KEY_ID, process.env.GLIA_KEY_SECRET);
-    const result = invokeGliaFunction(gliaBearerToken, invocationUri)
+    const result = invokeGliaFunction(gliaBearerToken, invocationUri, JSON.parse(data))
     console.log(result)
     return result
 }
 
-invokeFunctionCommand(argv.uri)
+invokeFunctionCommand(argv.uri, argv.data)
