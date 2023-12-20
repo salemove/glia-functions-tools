962aa20c9abe91c6d79ae292f1efcc5dbe6b9c44
Christian Steckler <christian.steckler@gmail.com>
2023-12-20 18:38:20 -0500

    referencing data from operator status webhook

--- examples/teams-presence/function.js
+++ examples/teams-presence/function.js
@@ -1,18 +1,42 @@
 export async function onInvoke(request, env) {
     try{
+      // request data
       const requestJson = await request.json();
       const payload = JSON.parse(requestJson.payload);
-      console.log('request payload= ', payload)
 
+      // TODO: Build out data mapping between Glia and Teams
       const mappingArray = [
         {
           
         }
       ]
-      return new Response(JSON.stringify({
-          input: payload,
-          output: ''
-      }));
+
+      // TODO: Get Microsoft access token
+
+      // TODO: Set Presence and Set Status Message
+
+      // find the operator ID and operator email
+      const operatorId = payload.operator.id;
+      const operatorEmail = payload.operator.email;
+
+      // find the availability status
+      const availabilityStatus = payload.availability_status;
+      console.log(`Agent ${operatorEmail} is ${availabilityStatus}`)
+
+      // also check [engagements] ?
+      if (availabilityStatus == 'available') {
+        // handle case where the agent is available
+
+      }
+
+      if (availabilityStatus == 'unavailable') {
+        // handle case where the agent is unavailable
+        // find the unavailability reason
+        const unavailabilityReasonCode = payload.unavailability_reason_code;
+        console.log(`Agent ${operatorEmail} is unavailable due to ${unavailabilityReasonCode}`)
+
+      }
+      return new Response(JSON.stringify({}));
     } catch(e) { 
       console.log(e); 
       return new Response(JSON.stringify({ error: e }))
