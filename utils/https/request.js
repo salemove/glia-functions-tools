6da2b3c4700aff1df05a743ff6002c8ba0bd4376
Christian Steckler <christian.steckler@gmail.com>
2024-02-09 09:10:30 -0500

    increased delay between creating new version and deploying new version in createAndDeploy script

--- utils/https/request.js
+++ utils/https/request.js
@@ -1,37 +1,12 @@
-import https from 'https'
-
 const request = async(url, options, data) => {
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(url, options, (res) => {
-      if (res.statusCode < 200 || res.statusCode > 299) {
-        console.log(`this request failed: ${url} ${JSON.stringify(options)} ${JSON.stringify(data)}`)
-        console.log('Error message: ', res.statusMessage)
-        return reject(new Error(`HTTP status code ${res.statusCode}`))
-      }
-
-      const body = []
-      res.on('data', (chunk) => body.push(chunk))
-      res.on('end', () => {
-        const resString = Buffer.concat(body).toString()
-        resolve(resString)
-      })
-    })
-
-    req.on('error', (err) => {
-      console.log(err)
-      reject(err)
-    })
-
-    req.on('timeout', () => {
-      req.destroy()
-      reject(new Error('Request time out'))
-    })
-    if (data) {
-        req.write(JSON.stringify(data));
-    }
-    req.end()
+  const { method, headers } = options;
+  const result = await fetch(url, {
+    method: method,
+    headers: headers,
+    body: JSON.stringify(data)
   })
+  const jsonResult = await result.json()
+  return JSON.stringify(jsonResult)
 }
 
 export default request
