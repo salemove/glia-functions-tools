260ba32d9b0e36dcf4399ef4bc12d8c589ffc567
Christian Steckler <christian.steckler@gmail.com>
2024-02-10 09:36:11 -0500

    use fetch and fetch logs recursively

--- utils/fetch-logs.js
+++ utils/fetch-logs.js
@@ -1,16 +1,28 @@
-import request from './https/request.js';
-
-const fetchGfLogs = async (functionId) => {
-    const result = await request(`${process.env.GLIA_API_URL}/functions/${functionId}/logs`, {
+const fetchGfLogsFunction = async (bearer, requestUrl, existingLogs) => {
+    console.log(requestUrl)
+    const logs = existingLogs || [];
+    const result = await fetch(requestUrl, {
         method: 'GET',
         headers: {
             'Accept': 'application/vnd.salemove.v1+json',
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.GLIA_BEARER_TOKEN}`
-        },
-        timeout: 9000, // in ms
+            'Authorization': `Bearer ${bearer}`
+        }
     })
-    return JSON.parse(result)
+    const parsed = await result.json();
+    parsed.logs.forEach(log => logs.push(log))
+    if (parsed.next_page != null) {
+        await fetchGfLogsFunction(bearer, parsed.next_page, logs)
+    }
+    return logs.sort(function(x, y){
+        return x.timestamp - y.timestamp;
+    })
+};
+
+const fetchGfLogs = async (functionId) => {
+    const initialRequestUrl = `${process.env.GLIA_API_URL}/functions/${functionId}/logs`;
+    const logs = await fetchGfLogsFunction(process.env.GLIA_BEARER_TOKEN, initialRequestUrl)
+    return logs
 };
 
 export default fetchGfLogs
