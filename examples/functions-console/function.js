9b3a08855046eb63b50c5b7456741278345cbb64
christians-git-user <christian.steckler@gmail.com>
2024-05-21 00:32:54 +0000

    boilerplate glia function with createBearerToken fn

--- examples/functions-console/function.js
+++ examples/functions-console/function.js
@@ -1,36 +1,46 @@
-// import chatCompletion from './openai/chatCompletion.js'
+const createBearerTokenPromise = (id, secret) => {
+  return fetch(`https://api.glia.com/operator_authentication/tokens`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.salemove.v1+json'
+      },
+      body: JSON.stringify({
+        api_key_id: id, 
+        api_key_secret: secret
+      })
+  })
+};
+
+const makeHeaders => bearerToken => {
+  return {
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.salemove.v1+json',
+    'Authorization': `Bearer ${bearerToken}`
+  }
+};
+
+const postPromise = (url, method, headers, data) => {
+  return fetch(
+      url,
+      {
+          method: method,
+          headers: headers,
+          body: JSON.stringify(data)
+      }
+  )
+};
 
 export async function onInvoke(request, env) {
     try{
         const requestJson = await request.json();
         const payload = JSON.parse(requestJson.payload);
-        // const result = await chatCompletion(env.OPENAI_SECRET, payload.content);
-        const result = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${env.OPENAI_SECRET}`
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [
-                  {
-                    role: "system",
-                    content: "You are a helpful assistant."
-                  },
-                  {
-                    role: "user",
-                    content: payload.content
-                  }
-                ]
-              })
-        }).then(res => res.json())
-        console.log(result)
-        return new Response(JSON.stringify({
-            input: payload,
-            // output: JSON.parse(result).choices[0].message.content
-            output: result.choices[0].message.content
-        }));
+        // payload: { url, method, data }
+        const bearer = await createBearerToken(env.GLIA_KEY_ID, env.GLIA_KEY_SECRET)
+        const headers = makeHeaders(bearer);
+        postPromise(payload.url, payload.method, headers, data)
+          .then(res = res.json())
+          .then(resultJson => return new Response(JSON.stringify(payload)))
     } catch(e) { 
         console.log(e); 
         return new Response(JSON.stringify({ error: e }))
