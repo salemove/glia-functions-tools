b8af84fc780723e8ed8cef1ad01a23f0b7aebb6a
Christian <christian.steckler@glia.com>
2024-06-27 21:07:05 +0000

    removed unused functions from index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import CLIIntro from './lib/cli/cliIntro.js';
 import CLIMainMenu from './lib/cli/cliMainMenu.js';
-import CLISetup from './lib/cli/cliSetup.js';
-import CLIAuth from './lib/cli/cliAuth.js';
-import CLIBuildMenu from './lib/cli/cliBuildMenu.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
