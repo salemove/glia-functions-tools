3541324f0f9628b705a4da30608ca49d6d80db6a
Brandon Wright <brandon.wright@glia.com>
2024-09-09 13:00:15 -0400

    Bug Fixes and Cleanup

--- examples/surveyToHubspotTicket/function.js
+++ examples/surveyToHubspotTicket/function.js
@@ -1,204 +1,171 @@
+// function.js
 async function apiCall(endpoint, method, body, token) {
     const url = `https://api.hubapi.com/${endpoint}`;
-    
     const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
     };
-
     const options = {
-        method: method,
-        headers: headers,
-        body: method !== "GET" ? JSON.stringify(body) : undefined 
+      method,
+      headers,
+      body: method !== "GET" ? JSON.stringify(body) : void 0
     };
-
     const response = await fetch(url, options);
-
-    // Handle network errors
     if (!response.ok) {
-        console.log("Response status: " + response.status);
-        console.log("Response text: " + response.statusText);
+      console.log("Response status: " + response.status);
+      console.log("Response text: " + response.statusText);
     }
-
     return await response.json();
-}
-
-async function createTicket(engagementHistory, engagementId, hubspotContactId, token) {
+  }
+  async function createTicket(engagementHistory, engagementId, hubspotContactId, token) {
     console.log("Creating ticket");
-
     const endpoint = "crm/v3/objects/tickets";
-    
     const body = {
-        properties: {
-            hs_pipeline_stage: 1,
-            subject: "Glia ticket from Survey "+engagementId,
-            content: `
-            Transcript: 
-            ${engagementHistory}
-            `
-        }
+      properties: {
+        hs_pipeline_stage: 1,
+        subject: "Glia ticket from Survey " + engagementId,
+        content: `
+              Transcript: 
+              ${engagementHistory}
+              `
+      }
     };
-
     if (hubspotContactId) {
-        body.associations = [];
-
-        body.associations.push( 
+      body.associations = [];
+      body.associations.push(
+        {
+          "to": {
+            "id": hubspotContactId
+          },
+          "types": [
             {
-                "to": {
-                    "id": hubspotContactId
-                },
-                "types": [
-                    {
-                        "associationCategory": "HUBSPOT_DEFINED",
-                        "associationTypeId": 16
-                    }
-                ]
-            });
-    }
-
-    try {
-        const data = await apiCall(endpoint, "POST", body, token);
-        return data;
-        
-    } catch (error) {
-        console.log("Data Error: ", data);
-        console.error("Error creating ticket:", error);
-        return false;
-    }
-}
-
-async function hubspotLookForEmail(token, email) {
-    if (email !== "") {
-        const endpoint = "crm/v3/objects/contacts/search";
-        
-        const body = {
-            filterGroups: [
-                {
-                    filters: [
-                        {
-                            propertyName: "email",
-                            value: email,
-                            operator: "EQ"
-                        }
-                    ]
-                }
-            ]
-        };
-
-        try {
-            const data = await apiCall(endpoint, "POST", body, token);
-            
-            if (data.results && data.results.length > 0) {
-                return data.results[0].id; // Make sure there's at least one result
-            } else {
-                console.log("Nothing found?");
-                return false; // Explicitly return false if no results
+              "associationCategory": "HUBSPOT_DEFINED",
+              "associationTypeId": 16
             }
-        } catch (error) {
-            console.error("Error retrieving data:", error);
-            return false; // Handle error: return false if API call fails
+          ]
         }
-    } else {
-        return false; // Return false for empty emails
+      );
     }
-}
-
-
-
-async function getAuthorizationToken(apiKeyId, apiKeySecret) {
-    const url = 'https://api.glia.com/operator_authentication/tokens';
-    const headers = {
-        'Accept': 'application/vnd.salemove.v1+json',
-        'Content-Type': 'application/json',
-    };
-    const body = JSON.stringify({
-        api_key_id: apiKeyId,
-        api_key_secret: apiKeySecret,
-    });
-    
+  
+    console.log("Creating ticket: " + JSON.stringify(body));
+  
     try {
-        const response = await fetch(url, { method: 'POST', headers: headers, body: body });
-        const data = await response.json();
-        
-        // Assuming the token is returned in a property named 'token' in the response
-        if (response.ok && data.token) {
-            return data.token;
-        } else {
-            console.error('Failed to get authorization token:', data);
-        }
+      const data2 = await apiCall(endpoint, "POST", body, token);
+      return data2;
     } catch (error) {
-        console.error('Error while fetching authorization token:', error);
+      console.log("Data Error: ", data);
+      console.error("Error creating ticket:", error);
+      return false;
     }
   }
+  async function hubspotLookForEmail(token, email) {
+    if (email !== "") {
+      const endpoint = "crm/v3/objects/contacts/search";
+      const body = {
+          "filterGroups": [
+              {
+                "filters": [
+                  {
+                    "propertyName": "email",
+                    "value": email,
+                    "operator": "EQ"
+                  }
+                ]
+              }
+          ]
+      };
   
-
-async function checkSurvey(token, engagementId, env)
-{
-    const url = `https://api.glia.com/engagements/${engagementId}/survey_answers/operator`
-
+      console.log("Looking for email: " + JSON.stringify(body, null, 2));
+      try {
+        const data2 = await apiCall(endpoint, "POST", body, token);
+        if (data2.results && data2.results.length > 0) {
+          return data2.results[0].id;
+        } else {
+          console.log("Nothing found: " + JSON.stringify(data2, null, 2));
+          return false;
+        }
+      } catch (error) {
+        console.error("Error retrieving data:", error);
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  async function getAuthorizationToken(apiKeyId, apiKeySecret) {
+    const url = "https://api.glia.com/operator_authentication/tokens";
     const headers = {
-        "Accept": "application/vnd.salemove.v1+json",
-        "Authorization": `Bearer ${token}`
+      "Accept": "application/vnd.salemove.v1+json",
+      "Content-Type": "application/json"
+    };
+    const body = JSON.stringify({
+      api_key_id: apiKeyId,
+      api_key_secret: apiKeySecret
+    });
+    try {
+      const response = await fetch(url, { method: "POST", headers, body });
+      const data2 = await response.json();
+      if (response.ok && data2.token) {
+        return data2.token;
+      } else {
+        console.error("Failed to get authorization token:", data2);
+      }
+    } catch (error) {
+      console.error("Error while fetching authorization token:", error);
+    }
+  }
+  async function checkSurvey(token, engagementId, env) {
+    const url = `https://api.glia.com/engagements/${engagementId}/survey_answers/operator`;
+    const headers = {
+      "Accept": "application/vnd.salemove.v1+json",
+      "Authorization": `Bearer ${token}`
     };
     try {
-        const response = await fetch(url, { method: 'GET', headers: headers })
-        const data = await response.json();
-
-        // loop through answers until we find the question "Open a Hubspot Ticket?"
-
-        if(response.ok && data.answers) {
-            for(let i = 0; i < data.answers.length; i++) {
-                if(data.answers[i].title === env.SURVEY_QUESTION) {
-                    if(data.answers[i].answer === true) {
-                        return true;
-                    }
-                }
+      const response = await fetch(url, { method: "GET", headers });
+      const data2 = await response.json();
+      if (response.ok && data2.answers) {
+        for (let i = 0; i < data2.answers.length; i++) {
+          if (data2.answers[i].title === env.SURVEY_QUESTION) {
+            if (data2.answers[i].answer === true) {
+              return true;
             }
-        } else {
-            console.log("Error while checking survey:", data);
-            return false;
-        }         
+          }
+        }
+      } else {
+        console.log("Error while checking survey:", data2);
+        return false;
+      }
     } catch (error) {
-        console.error("Error while checking survey:", error);
+      console.error("Error while checking survey:", error);
     }
-}
-
-async function onInvoke(request, env) {
+  }
+  async function onInvoke(request, env) {
     const requestJson = await request.json();
     const parsed = JSON.parse(JSON.parse(requestJson.payload));
-    // parsed contains information passed through the webhook
     const custom_fields = parsed["custom_fields"] || parsed.custom_fields;
     const engagement_id = parsed["engagement_id"] || parsed.engagement_id;
     const visitorEmail = parsed["visitorEmail"] || parsed.visitorEmail;
     const chat_transcript_plain_text = parsed["chat_transcript_plain_text"] || parsed.chat_transcript_plain_text;
-    const hubspotToken = env.HUBSPOT_ACCESS_TOKEN
-
+    const hubspotToken = env.HUBSPOT_ACCESS_TOKEN;
     const token = await getAuthorizationToken(env.GLIA_USER_API_KEY, env.GLIA_USER_SECRET);
     let hubspotContactId = "";
     if (custom_fields && custom_fields.hubspotContactId) {
       hubspotContactId = custom_fields.hubspotContactId;
     } else {
-        if(visitorEmail)
-        {
-            const hubspotContactId = await hubspotLookForEmail(hubspotToken, visitorEmail)
-            console.log("hubspotContactId: " + hubspotContactId);
-        } else {
-            hubspotContactId = "";
-        }
+      if (visitorEmail) {
+        hubspotContactId = await hubspotLookForEmail(hubspotToken, visitorEmail);
+      } else {
+  
+      }
     }
-
     const survey = await checkSurvey(token, engagement_id, env);
-
     if (survey) {
-        const ticket = await createTicket(chat_transcript_plain_text, engagement_id, hubspotContactId, hubspotToken);
-
-        console.log("Ticket: " + JSON.stringify(ticket));
+      const ticket = await createTicket(chat_transcript_plain_text, engagement_id, hubspotContactId, hubspotToken);
+      console.log("Ticket: " + JSON.stringify(ticket));
     }
-
-    // return status 201 for exports
     return new Response(JSON.stringify({ status: 201 }));
-    
-}
-export {
-  onInvoke
-};
+  }
+  export {
+    onInvoke
+  };
+  
